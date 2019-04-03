af312c2954cd0057b47c8ada2450734467d6bb45
andersnauman <anders.nauman@gmail.com>
2019-04-03 15:16:51 +0200

    Changed from dnf -> yumdownloader

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#sudo /usr/bin/dnf --downloaddir . --downloadonly reinstall python3-yubico python3-pyusb -y > /dev/null
-/usr/bin/dnf download python3-yubico python3-pyusb > /dev/null
+/usr/bin/yumdownloader python-yubico pyusb
 ls *.rpm
 
